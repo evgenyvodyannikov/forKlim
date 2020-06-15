8d8ad0cca3396f1ed375a86b92a122d8a07d453f
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-15 20:56:48 +0500

    OK.

--- Строки 15.06 Клим №2/Строки 15.06 Клим №2/Form1.cs
+++ Строки 15.06 Клим №2/Строки 15.06 Клим №2/Form1.cs
@@ -16,5 +16,22 @@ namespace Строки_15._06_Клим__2
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string result = null;
+            bool zapyataya = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= '0' && s[i] <= '9')
+                { result += s[i].ToString(); }
+                if (s[i] == ',')
+                { if (i != 0 && zapyataya == false)
+                    { result += s[i].ToString(); zapyataya = true; } 
+                }
+            }
+            decimal res = decimal.Parse(result);
+            textBox1.Text = res.ToString();
+        }
     }
 }
